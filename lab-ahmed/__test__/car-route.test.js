679f8d1700b5e42aebd9ec0868d53f7c227cf844
Ahmed Ali <Ahmed98148@gmail.com>
2018-02-21 15:41:48 -0800

    scaffold lecture

---
+++ lab-ahmed/__test__/car-route.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const request = require('superagent');
+require('jest');
+require('../server.js');
+
+describe('Car routes', function(){
+  var car = null;
+
+  describe('POST: /api/car', function(){
+    it('should return a car', function(done){
+      request.post('localhost:3000/api/car')
+      .send({name: 'test name', content: 'test content'})
+      .end((err,res) => {
+        if (err) return done(err);
+        car = JSON.parse(res.text);
+        console.log('res', res);
+        console.log('res text', res.text);
+        console.log('car', car);
+        expect(res.status).toEqual(200);
+        expect(car.name).toEqual('test name');
+        expect(car.content).toEqual('test content');
+        done();
+      });
+    });
+  });
+  describe('GET: /api/car', function(){
+    it('should return a car', function(done){
+      request.get(`localhost:3000/api/car?id=${car.id}`)
+      .end((err, res) => {
+        if(err) return done(err);
+        car = JSON.parse(res.text);
+        expect(res.status).toEqual(200)
+        expect(car.name).toEqual('test name');
+        expect(car.content).toEqual('test content');
+        done();
+      });
+    });
+  });
+});
