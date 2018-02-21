1589f9dd80e25b96f151413288332518c88f7d22
Katy Robinson <katyorob@gmail.com>
2018-02-21 15:33:34 -0800

    storage.js done

--- lab-katy/lib/router.js
+++ lab-katy/lib/router.js
@@ -30,3 +30,34 @@ Router.prototype.delete = function(endpoint, callback) {
   this.routes.DELETE[endpoint] = callback;
 };
 
+Router.prototype.route = function() {
+  return (req, res) => {
+    Promise.all([
+      parseUrl(req), 
+      parseJSON(req),
+    ])
+      .then( () => {
+        if (typeof this.routes[req.method][req.url.pathname] === 'function') {
+        //this is where the whole thing comes together
+          this.routes[req.method][req.url.pathname](req, res);
+          return;
+        }
+        console.error('route not found');
+
+        res.writeHead(404, {
+          'Content-Type': 'text/plain',
+        });
+        res.write('route not found');
+        res.end();
+      })
+      .catch( err => {
+        console.error(err);
+
+        res.writeHead(400, {
+          'Content-Type': 'text/plain',
+        });
+        res.write('bad request');
+        res.end();
+      });
+  };
+};
