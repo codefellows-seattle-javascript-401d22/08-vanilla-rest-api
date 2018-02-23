a8076b6f6f5b55a8852e8b52065278bf246e809d
Christian Miller <christian.ax.miller@gmail.com>
2018-02-22 22:09:40 -0800

    major refactor

--- lab-christian/lib/router.js
+++ lab-christian/lib/router.js
@@ -2,6 +2,7 @@
 
 const parseUrl = require('./parse-url.js');
 const parseJSON = require('./parse-json.js');
+const response = require('./response.js');
 
 const Router = module.exports = function() {
   this.routes = {
@@ -40,23 +41,12 @@ Router.prototype.route = function() {
           return;
         }
 
+        response.sendText(res, 404, 'route not found');
         console.error('route not found');
-
-        res.writeHead(404, {
-          'Content-Type': 'text/plain',
-        });
-        
-        res.write('route not found');
-        res.end();
       })
       .catch( (err) => {
+        response.sendText(res, 400, 'bad request');
         console.error(err);
-        res.writeHead(400, {
-          'Content-Type': 'text/plain',
-        });
-
-        res.write('bad request');
-        res.end();
       });
   };
 };
--- lab-christian/server.js
+++ lab-christian/server.js
@@ -1,79 +1,15 @@
 'use strict';
 
 const http = require('http');
-const Cat = require('./model/cat.js');
 const Router = require('./lib/router.js');
-const storage = require('./lib/storage.js');
+const catRoutes = require('./route/cat-route.js');
+
+
 const PORT = process.env.PORT || 3000;
 const router = new Router();
 
-router.get('/api/cat', function(req, res) {
-  if (req.url.query.id) {
-    storage.fetchItem('cat', req.url.query.id)
-      .then( cat => {
-        res.writeHead(200, {
-          'Content-Type': 'text/plain',
-        });
-        res.write(JSON.stringify(cat));
-        res.end();
-      })
-      .catch( err => {
-        console.error(err);
-        res.writeHead(404, {
-          'Content-Type': 'text/plain',
-        });
-        res.write('route not found');
-        res.end();
-      });
-    return;
-  }
-  res.writeHead(400, {
-    'Content-Type': 'text/plain',
-  });
-  res.write('bad request');
-  res.end();
-});
 
-router.delete('/api/cat', function(req, res) {
-  if (req.url.query.id) {
-    storage.deleteItem('cat', req.url.query.id)
-      .then( () => {
-        res.writeHead(204, {
-          'Content-Type': 'text/plain'
-        });
-        res.write('cat deleted');
-        res.end();
-      })
-      .catch( err => {
-        res.writeHead(400, {
-          'Content-Type': 'text/plain',
-        });
-        res.write('bad request');
-        res.end();
-      });
-  };
-});
-
-router.post('/api/cat', function(req, res) {
-  try {
-    var cat = new Cat(req.body.name, req.body.color);
-    storage.createItem('cat', cat);
-    res.writeHead(200, {
-      'Content-Type': 'text/plain',
-    });
-
-    res.write(JSON.stringify(cat));
-    res.end();
-  } catch (err) {
-    console.error(err);
-    res.writeHead(400, {
-      'Content-Type': 'text/plain',
-    });
-    res.write('bad request');
-    res.end();
-  }
-})
-
+catRoutes(router);
 const server = http.createServer(router.route());
 
 server.listen(PORT, () => {
